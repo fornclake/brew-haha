c5e5f13f132adb1dabb713605a0adb1fcca05a98
Joseph Lesko <mail@josephlesko.com>
2023-03-31 21:03:43 -0500

    highlight valid cell

--- main.gd
+++ main.gd
@@ -1,10 +1,24 @@
-extends Node2D
+class_name Main extends Node2D
 
-var valid_cells = []
+var valid_cells = [] # all interactable tiles on the grid
+
+# getters
+var player_nodes:
+	get: return get_tree().get_nodes_in_group("Players")
+var counter_tiles:
+	get: return tiles.get_used_cells_by_id(0, 0, Vector2i.ZERO)
 
 @onready var tiles = $TileMap
 
+
 func _ready():
-	for tile in tiles.get_used_cells_by_id(0, 0, Vector2i.ZERO):
+	# give all players a reference to this main scene
+	for player in player_nodes:
+		player.main = self
+	
+	# list all interactable tiles as valid cells
+	for tile in counter_tiles:
 		valid_cells.append(tile)
-	print(valid_cells)
+
+func cell_is_valid(cell : Vector2i) -> bool: # if cell is interactable
+	return cell in valid_cells
--- scripts/player.gd
+++ scripts/player.gd
@@ -1,20 +1,32 @@
 class_name Player extends CharacterBody2D
 
 const SPEED = 120
+const COLLISION_OFFSET = Vector2(16, 4)
+const CELL_SIZE = Vector2(32,32)
 
-var grid_position : Vector2i: # current position rounded to a 32*32 grid
-	get: return ((position + Vector2(12, 4)) / Vector2(32, 32)).round()
-
-var input_vector : Vector2: # vector of currently pressed arrow keys
-	get: return Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-
-var orth_direction : Vector2i: # last orthogonal vector player has faced
+var grid_position : Vector2: # current position rounded to a 32*32 grid
+	get: return ((position + COLLISION_OFFSET) / CELL_SIZE).round() - Vector2(1,0)
+var input_direction : Vector2: # vector of currently pressed arrow keys
+	get: return Input.get_vector("move_left", "move_right", "move_up", "move_down")
+var orth_direction : Vector2: # last orthogonal vector player has faced
 	get:
-		if input_vector in [Vector2.LEFT, Vector2.RIGHT, Vector2.UP, Vector2.DOWN]:
-			orth_direction = input_vector
+		if input_direction in [Vector2.LEFT, Vector2.RIGHT, Vector2.UP, Vector2.DOWN]:
+			orth_direction = input_direction
 		return orth_direction
+var selected_cell : Vector2: # cell player is facing
+	get: return grid_position + orth_direction
+
+var main : Main # set by Main on all nodes in Player group
+@onready var selector = $Selector
 
 
 func _physics_process(_delta):
-	velocity = input_vector * SPEED
+	velocity = input_direction * SPEED
 	move_and_slide()
+	
+	# show selector sprite only if player is facing a counter
+	if main.cell_is_valid(selected_cell):
+		selector.show()
+		selector.position = selected_cell * CELL_SIZE
+	else:
+		selector.hide()
